a235588d0b6defcdd22b42f0b6b023861ad17b08
kcx-001 <kcx-001@gmail.com>
2026-01-30 14:37:15 +0530

    cors issue solve

--- backend/src/app.js
+++ backend/src/app.js
@@ -36,7 +36,7 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.options("/.*/", cors());
 app.use(express.json({ limit: '10mb' })); // Limit JSON payload size
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
