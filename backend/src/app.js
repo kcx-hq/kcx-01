d6ad3bf6e82c464f35bdd656bcce2bd2ded22bd2
kcx-001 <kcx-001@gmail.com>
2026-02-03 14:27:40 +0530

    app cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,7 @@ app.use(compression({ level: 6, threshold: 1024 })); // Compress responses > 1KB
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // frontend (supports both ports)
+    origin: ["http://localhost:5173", "http://localhost:5174" , "https://master-01-kohl.vercel.app"], // frontend (supports both ports)
     credentials: true,               // allow cookies
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -64,7 +64,7 @@ const PORT = process.env.PORT || 5000;
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected successfully');
-   return sequelize.sync({ force:  true , alter: false}); 
+   return sequelize.sync({ force:  false , alter: false}); 
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
